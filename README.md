766e183a54bc63611f35652db820e4b82d952684
Jordan Johnson <jordanleej567@gmail.com>
2017-05-27 05:01:35 -0400

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # SharpHotkey
-SharpHotkey is a small library to easily add global hotkeys to your Windows Forms application.
+
+SharpHotkey is a small library to easily add global hotkeys to your Windows Forms application. Simply add the component file `SharpHotkey.dll` to your project references and follow the guide below to get started.
+
+## Usage
+
+```cs
+using SharpHotkey;
+
+namespace MyProject {
+	public partial class MainForm : Form {
+		private Hotkey _hk;
+
+		private void MainForm_Load(object sender, EventArgs e) {
+			_hk = new Hotkey(this, Keys.A, Hotkey.Modifiers.Ctrl + Hotkey.Modifiers.Shift);
+		}
+
+		protected override void WndProc(ref Message m) {
+			if(m.Msg == Hotkey.Message.Id) {
+				MessageBox.Show("Pressed!");
+			}
+
+			base.WndProc(ref m);
+		}
+	}
+}
+```
+
+## Summary
+
+In the usage above, I attached an on-load event to the form then I instantiated the `Hotkey` class by providing the current form, a standard key using the `Keys` enum, and the key modifiers (ctrl and shift). I also implemented `WndProc` to execute an action when the hotkey is used. A `MessageBox` will appear displaying `Pressed!`
+
+The hotkey will automatically deregister itself when the form closes.
+
+## Special Thanks
+
+Thank you to [Curtis Rutland](http://www.dreamincode.net/forums/topic/180436-global-hotkeys/) for explaining in great detail how everything works on the back-end. I made this library to simplify the process and will be using it in my upcoming project, SharpRevise.
---
+++ src/Hotkey.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace SharpHotkey {
+	public class Hotkey {
+		/// <summary>
+		/// Window handle (from form)
+		/// </summary>
+		private IntPtr _hWnd;
+
+		/// <summary>
+		/// Collection of modifiers (shift, alt, or ctrl)
+		/// </summary>
+		private int _modifier;
+
+		/// <summary>
+		/// Unique key id
+		/// </summary>
+		private int _id;
+
+		/// <summary>
+		/// Virtual key code
+		/// </summary>
+		private int _key;
+
+		/// <summary>
+		/// Imports hotkey registration method from user32.dll
+		/// <para>user32.dll is included with Windows.Forms</para>
+		/// </summary>
+		/// <param name="hWnd">Window handle (in this case, form)</param>
+		/// <param name="id">Key identifier</param>
+		/// <param name="fsModifiers">Shift, Alt, Ctrl, etc. modifiers</param>
+		/// <param name="vk">Virtual key code</param>
+		/// <returns></returns>
+		[DllImport("user32.dll")]
+		private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+		/// <summary>
+		/// Imports hotkey deregistration method from user32.dll
+		/// <para>user32.dll is included with Windows.Forms</para>
+		/// </summary>
+		/// <param name="hWnd"></param>
+		/// <param name="id"></param>
+		/// <returns></returns>
+		[DllImport("user32.dll")]
+		private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+		/// <summary>
+		/// Assigns a global hotkey
+		/// </summary>
+		/// <param name="form"></param>
+		/// <param name="key">Use Keys enum to assign</param>
+		/// <param name="modifier">Use Hotkey.Modifiers to assign</param>
+		public Hotkey(Form form, Keys key, int modifier) {
+			_hWnd = form.Handle;
+			_key = (int)key;
+			_modifier = modifier;
+			_id = this.GetHashCode();
+
+			// if registration unsuccessful
+			if(!Register()) {
+				throw new Exception("Global hotkey could not be registered");
+			}
+		}
+
+		/// <summary>
+		/// Deconstructor attempts to deregister global hotkey
+		/// </summary>
+		~Hotkey() {
+			if(!Deregister()) {
+				throw new Exception("Global hotkey could not be deregistered");
+			}
+		}
+
+		/// <summary>
+		/// Used in checking if hotkey is registered
+		/// </summary>
+		public bool IsRegistered = false;
+
+		/// <summary>
+		/// Modifiers for hotkey
+		/// <para>Available: Alt, Ctrl, Shift</para>
+		/// </summary>
+		public static class Modifiers {
+			public const int Alt = 0x0001;
+			public const int Ctrl = 0x0002;
+			public const int Shift = 0x0004;
+		}
+
+		/// <summary>
+		/// In your main form, override WndProc(ref Message m) and check if
+		/// the message matches the id. If matched, execute your action.
+		/// </summary>
+		public static class Message {
+			public const int Id = 0x0312;
+		}
+
+		/// <summary>
+		/// Registers global hotkey
+		/// <para>This method is called within the constructor</para>
+		/// </summary>
+		/// <returns>True if hotkey is registered</returns>
+		public bool Register() {
+			IsRegistered = RegisterHotKey(_hWnd, _id, _modifier, _key);
+
+			return IsRegistered;
+		}
+
+		/// <summary>
+		/// Deregisters global hotkey
+		/// <para>Run this method in your form closing event</para>
+		/// </summary>
+		/// <returns>True if hotkey is deregistered</returns>
+		public bool Deregister() {
+			bool success = UnregisterHotKey(_hWnd, _id);
+
+			IsRegistered = (success) ? false : true;
+
+			return success;
+		}
+
+		/// <summary>
+		/// Creates the unique id from the combination of modifiers and key
+		/// </summary>
+		/// <returns></returns>
+		public override int GetHashCode() {
+			return _modifier ^ _key ^ _hWnd.ToInt32();
+		}
+	}
+}
